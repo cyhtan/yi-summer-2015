2bab91e9a4502b6d0bfa21fa994763f2a4366f32
Faraji Starks <farajistarks@gmail.com>
2015-06-14 17:47:44 -0400

    add trackerRewriteQuickDirty, add forLoops.html, add forLoops.js

---
+++ jsfundamentals/forLoops.js
@@ -0,0 +1,97 @@
+var number = prompt('input favorite number!');
+var myArray = [1, 2, 3];
+
+function myFunction() {
+    myArray.push(i);
+}
+
+for (var i = 1; i <= number; i++) {
+    myArray = myArray + myFunction(i);
+
+//         // if (i = number) {
+//         // 	reduce(storage);
+//         // }
+        
+}
+console.log(myArray);
+
+//push values to string, reduce string values
+
+
+
+// var myarray = [5, 10, 3, 2];
+
+// var result = myarray.concat();
+
+
+//     for (var i = 0; i < myarray.length; i++){
+//         result[i] = myarray.slice(0, i + 1).reduce(function(p, i){ return p + i; });
+//     }
+
+// console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//asterisk example loop:
+
+// var number = prompt('Please enter your favorite number');
+
+// var star = '';
+
+// for (var i = 0; i < number; i++) {
+// 	star = star + '*';
+// 	console.log(star);
+// }
+// -------------------------------------
+// var i = 0;
+
+// for (var i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+
+// var sum = 0;
+// var number = prompt('Please enter your favorite number');
+
+// for (var i = 0; i < number; i++){
+// 	sum = sum + i;
+// }
+
+// console.log(sum);
