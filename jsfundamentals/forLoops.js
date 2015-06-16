1d89726d44992b5f4e57c4a4560d1dd383aa55f0
Faraji Starks <farajistarks@gmail.com>
2015-06-16 08:15:23 -0400

    Add forLoops exercise 1 and 2

--- jsfundamentals/forLoops.js
+++ jsfundamentals/forLoops.js
@@ -1,75 +1,18 @@
-var number = prompt('input favorite number!');
-var myArray = [1, 2, 3];
+// function calculateSum(number) {
+// 	var sum = 0;
+// 	for (var i = 1; i <= number; i++) {
+// 		sum = sum + i;
+// 	}         
+//     return sum;   
+// }
 
-function myFunction() {
-    myArray.push(i);
-}
+//---------------------------------------
+// var i = 0;
 
-for (var i = 1; i <= number; i++) {
-    myArray = myArray + myFunction(i);
-
-//         // if (i = number) {
-//         // 	reduce(storage);
-//         // }
-        
-}
-console.log(myArray);
-
-//push values to string, reduce string values
-
-
-
-// var myarray = [5, 10, 3, 2];
-
-// var result = myarray.concat();
-
-
-//     for (var i = 0; i < myarray.length; i++){
-//         result[i] = myarray.slice(0, i + 1).reduce(function(p, i){ return p + i; });
-//     }
-
-// console.log(result);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// for (var i = 0; i < 10; i++) {
+// 	console.log(i);
+// }
+// --------------------------------------
 //asterisk example loop:
 
 // var number = prompt('Please enter your favorite number');
@@ -81,17 +24,42 @@ console.log(myArray);
 // 	console.log(star);
 // }
 // -------------------------------------
-// var i = 0;
 
-// for (var i = 0; i < 10; i++) {
-// 	console.log(i);
+//helper function - calculate sum
+// function calculateSum (number) {
+//   var sum = 0;
+//   for (var i = 1; i <=number; i++) {
+//     sum = sum + i;
+//   }
+//   return sum;
+// };
+//-------------------------------------
+// var store =  '';
+// var input = prompt('enter favorite number');
+
+// for (var i = 0; i <=input; i++) {
+//   store = store + i;
+//   console.log(store)
+// };
+//------------------------------------
+
+//'exercise 1'
+var celebration = 'Hooray!';
+var scold = 'WRONGGGGGOOOO! an integer that is > 0!';
+var input = -1;
+
+for (;input < 0;) {
+  input = prompt('Enter an integer that is > 0!');
+  alert(scold);
+}
+alert(celebration);
+
+//------------------------------------
+//'exercise 2'
+// var answer = prompt('Do you like tofu?');
+
+// (answer === 'yes') ? alert('cool!') : prompt(tofu)
+
+// for (var i = 0; i < length; i++) {
+
 // }
-
-// var sum = 0;
-// var number = prompt('Please enter your favorite number');
-
-// for (var i = 0; i < number; i++){
-// 	sum = sum + i;
-// }
-
-// console.log(sum);
